04e4f17d5a8a746ea35dabfe27e19032b3a3aedb
Isaac Shapira <fresheyeball@gmail.com>
2014-03-09 18:00:46 -0600

    it swims!

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera : MonoBehaviour {
+
+	public Transform target;
+	public float distance;
+	
+	void Update () {
+		transform.position = new Vector3 (target.position.x, transform.position.y, target.position.z - distance);		
+	}
+}
---
+++ Assets/Scripts/Fish.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fish : MonoBehaviour {
+
+	public float speed;
+	public float turnSmoothing = 15f;
+	public float speedDampTime = 0.1f;
+
+	private Animator anim;
+
+	void Awake() {
+		anim = GetComponent<Animator> ();
+	}
+		
+	void FixedUpdate () {
+		float h = Input.GetAxis ("Horizontal");
+		float v = Input.GetAxis ("Vertical");
+
+		Vector3 moveit 	     = new Vector3 (h, 0.0f, v);
+
+		rigidbody.velocity = (moveit * speed);
+
+		anim.SetFloat ("speed", h*h+v*v);
+	}
+}
